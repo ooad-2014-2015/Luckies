6815c73cf0c6dacbda5c70f5bf1759a2297a2ea6
mirelapeskovic <mirelapeskovic@gmail.com>
2015-05-30 22:20:47 +0200

    Windows Phone aplikacija

---
+++ ZombieHuntWPhone/ZombieHuntWPhone/Page1.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ZombieHuntWPhone
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (ZombieHuntWindowsPhoneContext sdf = new ZombieHuntWindowsPhoneContext(ZombieHuntWindowsPhoneContext.ConnectionString))
+            {
+                sdf.CreateIfNotExists();
+                
+                try
+                {
+                  
+                    Table<Komentari> stranice = sdf.GetTable<Komentari>();
+                    Komentari k= new Komentari();
+                    k.Komentar = noviKomentar.Text;
+                    k.Datum = DateTime.Now;
+                    sdf.Komentari.InsertOnSubmit(k);
+                    sdf.SubmitChanges();
+
+                }
+                catch (Exception ex)
+                {
+
+                }       
+                noviKomentar.Text = "Uspjesno ste dodali komentar!";
+
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        }
+    }
---
+++ ZombieHuntWPhone/ZombieHuntWPhone/Page2.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ZombieHuntWPhone
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            using (ZombieHuntWindowsPhoneContext sdf = new ZombieHuntWindowsPhoneContext(ZombieHuntWindowsPhoneContext.ConnectionString))
+            {
+                sdf.CreateIfNotExists();
+                try
+                {
+                        Table<Komentari> komentari = sdf.GetTable<Komentari>();
+                        Table<Slike> slike = sdf.GetTable<Slike>();
+
+                        var komentariQuery = from k in komentari.ToList() select k;
+                        var slikeQuery = from s in slike.ToList() select s;
+
+                       
+                        foreach (var komentar in komentariQuery)
+                        {
+
+                            PivotItem p = new PivotItem();
+                            MyUserControl kontrola = new MyUserControl();
+                            kontrola.komentarUser.Text = komentar.Komentar;
+                            kontrola.datumUser.Text = komentar.Datum.ToString();
+
+                            Random rnd = new Random();
+                            int broj_slike = rnd.Next(1,6);
+
+                            foreach (var slika in slikeQuery)
+                            {
+                                if (slika.Id == broj_slike)
+                                {
+                                    if (slika.Slika.ToArray() != null && slika.Slika.ToArray() is Byte[])
+                                    {
+                                        MemoryStream stream = new MemoryStream(slika.Slika.ToArray());
+                                        BitmapImage image = new BitmapImage();
+                                        image.SetSource(stream);
+                                        kontrola.randomSlika.Source = image;
+                                    }
+
+                                    break;
+                                }
+                            }
+
+                            p.Header = "Komentar " + komentar.Id;
+                            p.Content = kontrola;
+                     
+                            pivot.Items.Add(p);
+                        }
+                
+                    
+                }
+                catch (Exception et)
+                {
+
+                }
+            }
+
+        }
+
+        private void PhoneApplicationPage_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+
+}
